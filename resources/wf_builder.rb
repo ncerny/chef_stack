2c798f31e3ea6a9303306792b9e153dc7b59ba31
Nathan Cerny <ncerny@chef.io>
2017-05-12 10:01:55 -0500

    fix typo.
    Signed-off-by: Nathan Cerny <ncerny@chef.io>

--- resources/wf_builder.rb
+++ resources/wf_builder.rb
@@ -75,7 +75,7 @@ action :create do
       @cacert_pem = ::File.read('/opt/chefdk/embedded/ssl/certs/cacert.pem')
       ::Dir.glob('/etc/chef/trusted_certs/*.crt').each do |crt|
         c = ::File.read(crt)
-        @cacert_pem << "\n" << c unless cacert_pem.include?(c)
+        @cacert_pem << "\n" << c unless @cacert_pem.include?(c)
       end
     end
   end
